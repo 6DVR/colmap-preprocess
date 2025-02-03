505104ee2e12b7c733dc0f64975fa55b9c048d54
Ishan Arya <ishan.arya130@gmail.com>
2025-02-03 19:30:29 +0530

    check

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import numpy.typing as NPT
+import numpy.testing as nptest
 from pathlib import Path
 from camera import getProjectionMatrix, getWorld2View2
 from models import CameraInfo
@@ -59,8 +60,8 @@ def _list_scenes(dataset_dir: str) -> list[Path]:
     return second_tier
 
 
-def run():
-    path = "/Users/eos/Downloads/datasets/mvimgnet"
+def run(path: str):
+    # path = "/Users/eos/Downloads/datasets/mvimgnet"
     pose_folder = "poses"
     scenes = _list_scenes(path)
     with open(os.path.join(path, "scenes_list.txt"), "w") as f:
@@ -78,6 +79,30 @@ def run():
                 pose_encoding = calc_scene_pose(camera)
                 np.save(pose_file_path, pose_encoding)
 
+def check(path: str):
+    # path = "/Users/eos/Downloads/datasets/mvimgnet"
+    pose_folder = "poses"
+    scenes = _list_scenes(path)
+
+    scene_list = set(
+        open(os.path.join(path, "scenes_list.txt"), "r").read().splitlines()
+    )
+
+    for scene in scenes:
+        scene_path = os.path.join(path, scene)
+        ce, ci = get_cam_matrices(scene_path)
+        if ce is None or ci is None:
+            continue
+        assert scene in scene_list
+        scene_info = readColmapSceneInfo(scene_path)
+        for camera in scene_info.cameras:
+            pose_file_name = f"pe_{camera.image_name}.npy"
+            pose_file_path = os.path.join(scene_path, pose_folder, pose_file_name)
+            assert os.path.exists(pose_file_path)
+            pose_encoding_calc = calc_scene_pose(camera)
+            pose_encoding_load = np.load(pose_file_path)
+            nptest.assert_array_equal(pose_encoding_calc, pose_encoding_load)
+
 
 if __name__ == "__main__":
-    run()
+    check()
